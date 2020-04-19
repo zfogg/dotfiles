854c5562c595e1deff278c11a24bcdbc7ddfabab
Zachary Fogg <me@zfo.gg>
2020-04-19 03:15:39 -0400

    fixed paths finally. also changed router local domain

--- .config/zsh/z/path.zsh
+++ .config/zsh/z/path.zsh
@@ -39,7 +39,7 @@ function () { # {{{ platform-specifics
       $BREW/opt/fzf/bin
       $BREW/{sbin,bin}
       "$path[@]"
-      $(getconf PATH | command -p tr ':' '\n' | command -p tail -r))
+      $(getconf PATH | command -p tr ':' '\n'))
 
     fpath=(
       $BREW/share/zsh/{site-functions,functions}
@@ -71,7 +71,6 @@ function () { # {{{ platform-specifics
   fi
 } # }}}
 
-
 function() { # {{{ AFTER platform-specifics
   path=(
     $HOME/bin
@@ -79,7 +78,7 @@ function() { # {{{ AFTER platform-specifics
     $HOME/.{cabal,cargo,gem}/bin
     # NOTE: PYENV_ROOT+PATH are set by pyenv-lazy via antigen
     #$PYENV_ROOT/{bin,shims}
-    {$GOPATH,$GOROOT}/bin
+    $GOPATH/bin
     "$path[@]")
 
   fpath=(
