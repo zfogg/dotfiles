ae11993f758352807b80005e69619c70ca29c01c
Zachary Fogg <me@zfo.gg>
2022-04-09 18:03:44 -0400

    okay now indentation REALLY sucks

--- .vim/lua/rc/fern.lua
+++ .vim/lua/rc/fern.lua
@@ -14,157 +14,157 @@ function M.setup()
   vim.cmd [[
   if PHas('fern-renderer-nerdfont.vim')
     let g:fern#renderer = 'nerdfont'
-    endif
+  endif
 
-    let g:fern#disable_viewer_auto_duplication=1 " default=0
+  let g:fern#disable_viewer_auto_duplication=1 " default=0
 
-    let g:fern#disable_drawer_tabpage_isolation=1 " default=0
+  let g:fern#disable_drawer_tabpage_isolation=1 " default=0
 
-    let g:fern#disable_drawer_tabpage_isolation=200 " default=0
+  let g:fern#disable_drawer_tabpage_isolation=200 " default=0
 
-    let g:fern#drawer_width=24 " default=30
+  let g:fern#drawer_width=24 " default=30
 
-    " INFO: https://github.com/lambdalisue/fern.vim
+  " INFO: https://github.com/lambdalisue/fern.vim
 
-    aug glyph-palette-custom
-      au! *
-      au FileType fern call glyph_palette#apply()
-      au FileType nerdtree,startify call glyph_palette#apply()
-    aug END
+  aug glyph-palette-custom
+    au! *
+    au FileType fern call glyph_palette#apply()
+    au FileType nerdtree,startify call glyph_palette#apply()
+  aug END
 
 
-    "let g:fern_custom_isFloating = v:false
-    "function! Fern_ShowFloatingPreview()
-    "if g:fern_custom_isFloating == v:false
-    "let g:fern_custom_isFloating = v:true
-    "call fern#action#call('preview:auto:toggle')
-    "endif
-    "endfunction
-    "function! Fern_ToggleFloatingPreview()
-    "call fern#action#call('preview:toggle')
-    "endfunction
-    "function! Fern_AutoToggleFloatingPreview()
-    "if g:fern_custom_isFloating == v:true
-    "let g:fern_custom_isFloating = v:false
-    "else
-    "let g:fern_custom_isFloating = v:true
-    "endif
-    "call fern#action#call('preview:auto:toggle')
-    "endfunction
+  "let g:fern_custom_isFloating = v:false
+  "function! Fern_ShowFloatingPreview()
+  "if g:fern_custom_isFloating == v:false
+  "let g:fern_custom_isFloating = v:true
+  "call fern#action#call('preview:auto:toggle')
+  "endif
+  "endfunction
+  "function! Fern_ToggleFloatingPreview()
+  "call fern#action#call('preview:toggle')
+  "endfunction
+  "function! Fern_AutoToggleFloatingPreview()
+  "if g:fern_custom_isFloating == v:true
+  "let g:fern_custom_isFloating = v:false
+  "else
+  "let g:fern_custom_isFloating = v:true
+  "endif
+  "call fern#action#call('preview:auto:toggle')
+  "endfunction
 
-    function! s:init_fern() abort
-      nmap <buffer> o  <Plug>(fern-action-open:edit)
-      nmap <buffer> go <Plug>(fern-action-open:edit)<C-w>p
-      nmap <buffer> t  <Plug>(fern-action-open:tabedit)
-      nmap <buffer> T  <Plug>(fern-action-open:tabedit)gT
-      nmap <buffer> i  <Plug>(fern-action-open:split)
-      nmap <buffer> gi <Plug>(fern-action-open:split)<C-w>p
-      nmap <buffer> s  <Plug>(fern-action-open:vsplit)
-      nmap <buffer> gs <Plug>(fern-action-open:vsplit)<C-w>p
-      nmap <buffer> ma <Plug>(fern-action-new-path)
-      nmap <buffer> P  gg
+  function! s:init_fern() abort
+    nmap <buffer> o  <Plug>(fern-action-open:edit)
+    nmap <buffer> go <Plug>(fern-action-open:edit)<C-w>p
+    nmap <buffer> t  <Plug>(fern-action-open:tabedit)
+    nmap <buffer> T  <Plug>(fern-action-open:tabedit)gT
+    nmap <buffer> i  <Plug>(fern-action-open:split)
+    nmap <buffer> gi <Plug>(fern-action-open:split)<C-w>p
+    nmap <buffer> s  <Plug>(fern-action-open:vsplit)
+    nmap <buffer> gs <Plug>(fern-action-open:vsplit)<C-w>p
+    nmap <buffer> ma <Plug>(fern-action-new-path)
+    nmap <buffer> P  gg
 
-      nmap <buffer> C      <Plug>(fern-action-enter)
-      nmap <buffer> u      <Plug>(fern-action-leave)
-      nmap <buffer> r      <Plug>(fern-action-reload)
-      nmap <buffer> R    gg<Plug>(fern-action-reload)<C-o>
-      nmap <buffer> cd     <Plug>(fern-action-cd)
-      nmap <buffer> CD   gg<Plug>(fern-action-cd)<C-o>
+    nmap <buffer> C      <Plug>(fern-action-enter)
+    nmap <buffer> u      <Plug>(fern-action-leave)
+    nmap <buffer> r      <Plug>(fern-action-reload)
+    nmap <buffer> R    gg<Plug>(fern-action-reload)<C-o>
+    nmap <buffer> cd     <Plug>(fern-action-cd)
+    nmap <buffer> CD   gg<Plug>(fern-action-cd)<C-o>
 
-      nmap <buffer> I <Plug>(fern-action-hidden-toggle)
+    nmap <buffer> I <Plug>(fern-action-hidden-toggle)
 
-      nmap <buffer> q :<C-u>quit<CR>
+    nmap <buffer> q :<C-u>quit<CR>
 
-      " Use 'select' instead of 'edit' for default 'open' action
-      "nmap <buffer> <Plug>(fern-action-open) <Plug>(fern-action-open:select)
-      nmap <buffer> <Plug>(fern-action-open) <Plug>(fern-action-open:right)
-      " Let you move to a right split lol
-      silent! unmap <buffer> <C-l>
-      silent! unmap <buffer> <C-h>
+    " Use 'select' instead of 'edit' for default 'open' action
+    "nmap <buffer> <Plug>(fern-action-open) <Plug>(fern-action-open:select)
+    nmap <buffer> <Plug>(fern-action-open) <Plug>(fern-action-open:right)
+    " Let you move to a right split lol
+    silent! unmap <buffer> <C-l>
+    silent! unmap <buffer> <C-h>
 
-      nmap <buffer><expr>
-      \ <Plug>(fern-my-expand-or-collapse)
-      \ fern#smart#leaf(
-      \   "\<Plug>(fern-action-collapse)",
-      \   "\<Plug>(fern-action-expand)",
-      \   "\<Plug>(fern-action-collapse)",
-      \ )
+    nmap <buffer><expr>
+    \ <Plug>(fern-my-expand-or-collapse)
+    \ fern#smart#leaf(
+    \   "\<Plug>(fern-action-collapse)",
+    \   "\<Plug>(fern-action-expand)",
+    \   "\<Plug>(fern-action-collapse)",
+    \ )
 
-      "nmap <buffer><nowait> l <Plug>(fern-my-expand-or-collapse)
+    "nmap <buffer><nowait> l <Plug>(fern-my-expand-or-collapse)
 
-      nmap <buffer><expr>
-      \ <Plug>(fern-my-expand-or-enter)
-      \ fern#smart#drawer(
-      \   "\<Plug>(fern-action-open-or-expand)",
-      \   "\<Plug>(fern-action-open-or-enter)",
-      \ )
-      nmap <buffer><expr>
-      \ <Plug>(fern-my-collapse-or-leave)
-      \ fern#smart#drawer(
-      \   "\<Plug>(fern-action-collapse)",
-      \   "\<Plug>(fern-action-leave)",
-      \ )
-      nmap <buffer><nowait> l <Plug>(fern-my-expand-or-enter)
-      nmap <buffer><nowait> h <Plug>(fern-my-collapse-or-leave)
+    nmap <buffer><expr>
+    \ <Plug>(fern-my-expand-or-enter)
+    \ fern#smart#drawer(
+    \   "\<Plug>(fern-action-open-or-expand)",
+    \   "\<Plug>(fern-action-open-or-enter)",
+    \ )
+    nmap <buffer><expr>
+    \ <Plug>(fern-my-collapse-or-leave)
+    \ fern#smart#drawer(
+    \   "\<Plug>(fern-action-collapse)",
+    \   "\<Plug>(fern-action-leave)",
+    \ )
+    nmap <buffer><nowait> l <Plug>(fern-my-expand-or-enter)
+    nmap <buffer><nowait> h <Plug>(fern-my-collapse-or-leave)
 
-      " INFO: https://github.com/lambdalisue/fern.vim/wiki/Tips
-      nmap <buffer> <Plug>(fern-my-enter-and-tcd)
-      \ <Plug>(fern-action-enter)
-      \ <Plug>(fern-wait)
-      \ <Plug>(fern-action-tcd:root)
+    " INFO: https://github.com/lambdalisue/fern.vim/wiki/Tips
+    nmap <buffer> <Plug>(fern-my-enter-and-tcd)
+    \ <Plug>(fern-action-enter)
+    \ <Plug>(fern-wait)
+    \ <Plug>(fern-action-tcd:root)
 
-      nmap <buffer> <Plug>(fern-my-leave-and-tcd)
-      \ <Plug>(fern-action-leave)
-      \ <Plug>(fern-wait)
-      \ <Plug>(fern-action-tcd:root)
+    nmap <buffer> <Plug>(fern-my-leave-and-tcd)
+    \ <Plug>(fern-action-leave)
+    \ <Plug>(fern-wait)
+    \ <Plug>(fern-action-tcd:root)
 
-      "nmap <buffer><expr>
-      "\ <Plug>(fern-floating-preview-or-nop)
-      "\ fern#smart#leaf(
-      "\   ":silent! call Fern_ShowFloatingPreview()\<CR>",
-      "\   "",
-      "\ )
-      "nmap <buffer><expr>
-      "\ <Plug>(fern-floating-preview-or-nop)
-      "\ fern#smart#leaf(
-      "\   "\<Plug>(fern-action-preview:toggle)",
-      "\   "",
-      "\ )
+    "nmap <buffer><expr>
+    "\ <Plug>(fern-floating-preview-or-nop)
+    "\ fern#smart#leaf(
+    "\   ":silent! call Fern_ShowFloatingPreview()\<CR>",
+    "\   "",
+    "\ )
+    "nmap <buffer><expr>
+    "\ <Plug>(fern-floating-preview-or-nop)
+    "\ fern#smart#leaf(
+    "\   "\<Plug>(fern-action-preview:toggle)",
+    "\   "",
+    "\ )
 
-      "nmap <buffer><expr>
-      "\ <Plug>(fern-my-preview-or-nop)
-      "\ fern#smart#leaf(
-      "\   "\<Plug>(fern-action-open:edit)\<C-w>p",
-      "\   "",
-      "\ )
+    "nmap <buffer><expr>
+    "\ <Plug>(fern-my-preview-or-nop)
+    "\ fern#smart#leaf(
+    "\   "\<Plug>(fern-action-open:edit)\<C-w>p",
+    "\   "",
+    "\ )
 
-      nmap <buffer><expr> j
-      \ fern#smart#drawer(
-      \   "j\<Plug>(fern-floating-preview-or-nop)",
-      \   "j",
-      \ )
-      nmap <buffer><expr> k
-      \ fern#smart#drawer(
-      \   "k\<Plug>(fern-floating-preview-or-nop)",
-      \   "k",
-      \ )
+    nmap <buffer><expr> j
+    \ fern#smart#drawer(
+    \   "j\<Plug>(fern-floating-preview-or-nop)",
+    \   "j",
+    \ )
+    nmap <buffer><expr> k
+    \ fern#smart#drawer(
+    \   "k\<Plug>(fern-floating-preview-or-nop)",
+    \   "k",
+    \ )
 
-      nmap <silent> <buffer> p     <Plug>(fern-action-preview:toggle)
-      "nmap <silent> <buffer> p     :call Fern_ToggleFloatingPreview()<CR>
-      nmap <silent> <buffer> <C-p> <Plug>(fern-action-preview:auto:toggle)
-      "nmap <silent> <buffer> <C-p> :call Fern_AutoToggleFloatingPreview()<CR>
-      nmap <silent> <buffer> <C-d> <Plug>(fern-action-preview:scroll:down:half)
-      nmap <silent> <buffer> <C-u> <Plug>(fern-action-preview:scroll:up:half)
+    nmap <silent> <buffer> p     <Plug>(fern-action-preview:toggle)
+    "nmap <silent> <buffer> p     :call Fern_ToggleFloatingPreview()<CR>
+    nmap <silent> <buffer> <C-p> <Plug>(fern-action-preview:auto:toggle)
+    "nmap <silent> <buffer> <C-p> :call Fern_AutoToggleFloatingPreview()<CR>
+    nmap <silent> <buffer> <C-d> <Plug>(fern-action-preview:scroll:down:half)
+    nmap <silent> <buffer> <C-u> <Plug>(fern-action-preview:scroll:up:half)
 
-      nmap <silent> <buffer> <expr> <Plug>(fern-quit-or-close-preview) fern_preview#smart_preview("\<Plug>(fern-action-preview:close)", ":q\<CR>")
-      nmap <silent> <buffer> q <Plug>(fern-quit-or-close-preview)
-    endfunction
+    nmap <silent> <buffer> <expr> <Plug>(fern-quit-or-close-preview) fern_preview#smart_preview("\<Plug>(fern-action-preview:close)", ":q\<CR>")
+    nmap <silent> <buffer> q <Plug>(fern-quit-or-close-preview)
+  endfunction
 
-    augroup fern-custom
-    autocmd! *
-    autocmd FileType fern call s:init_fern()
-    autocmd FileType fern call fzf#vim#with_preview()
-    augroup END
+  aug fern-rc-custom
+    au! *
+    au FileType fern call s:init_fern()
+    au FileType fern call fzf#vim#with_preview()
+  aug END
 
   function! Fern_mapping_fzf_customize_option(spec)
     let a:spec.options .= ' --multi'
