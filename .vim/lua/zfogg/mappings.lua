eece7a91c34527e198e07f52fe924aee3dc795b0
Zachary Fogg <me@zfo.gg>
2025-03-21 10:20:40 -0400

    autoreload kitty conf on save in neovim and remove command-s mapping from kitty cuz neovim can handle it

--- .vim/lua/zfogg/mappings.lua
+++ .vim/lua/zfogg/mappings.lua
@@ -1,10 +1,10 @@
 -- lua/zfogg/mappins.lua
 local cmd = vim.cmd
 local o_s = vim.o
-local map_key = vim.api.nvim_set_keymap
 
-vim.keymap.set({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr>", { desc = "Save file" })
-vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr>", { desc = "Save file" })
+vim.keymap.set({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<CR>", { desc = "Save file" })
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
+--vim.keymap.set({ "i" }, ",W", "<Esc>:w<CR>a", { desc = "Save file" })
 
 vim.cmd [[
   "command! -nargs=+ -complete=command Redir let s:reg = @@ | redir @"> | silent execute <q-args> | redir END | new | pu | 1,2d_ | let @@ = s:reg
--- .vim/lua/zfogg/settings.lua
+++ .vim/lua/zfogg/settings.lua
@@ -4,3 +4,15 @@
 -- INFO: :help g:do_filetype_lua
 vim.g.do_filetype_lua    = 1
 vim.g.did_load_filetypes = 0
+
+local kitty_augroup      = vim.api.nvim_create_augroup("kitty_conf", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern  = "*/.config/kitty/*.conf",
+  group    = kitty_augroup,
+  callback = function()
+    vim.cmd [[
+      silent !pkill -USR1 kitty
+      redraw!
+    ]]
+  end,
+})
