747f20a611c6d311769c19b4dd0430c45cc10f66
Zachary Fogg <me@zfo.gg>
2025-03-21 05:40:55 -0400

    fixed bugs in my zsh startup

--- .config/zsh/z/complete.zsh
+++ .config/zsh/z/complete.zsh
@@ -278,5 +278,5 @@ function() {
 
 # tabtab source for packages
 # uninstall by removing these lines
-[[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
+#[[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
 
