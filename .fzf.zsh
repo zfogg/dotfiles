56fc0aa675743fcc57994834d3478dcd42af862f
Zachary Fogg <me@zfo.gg>
2025-07-02 19:31:04 -0700

    fix fzf and asdf for WorkBook-Pro

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/loomen/src/github.com/zfogg/dotfiles/.local/share/nvim/site/pack/packer/start/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/Users/loomen/src/github.com/zfogg/dotfiles/.local/share/nvim/site/pack/packer/start/fzf/bin"
+if [[ ! "$PATH" == *$HOME/.local/share/nvim/site/pack/packer/start/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}$HOME/.local/share/nvim/site/pack/packer/start/fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-source "/Users/loomen/src/github.com/zfogg/dotfiles/.local/share/nvim/site/pack/packer/start/fzf/shell/completion.zsh"
-
-# Key bindings
-# ------------
-source "/Users/loomen/src/github.com/zfogg/dotfiles/.local/share/nvim/site/pack/packer/start/fzf/shell/key-bindings.zsh"
+source <(fzf --zsh)
