8e957c5da795486c3b3875e13038be8cad504e24
Zachary Fogg <me@zfo.gg>
2026-01-15 23:02:00 -0500

    some settings from windwp/nvim-autopairs readme

--- .vim/lua/rc/coq.lua
+++ .vim/lua/rc/coq.lua
@@ -61,6 +61,46 @@ function M.config()
   --  })
 
   keymaps()
+
+  if vim.fn.PHas('nvim-autopairs') == 1 then
+    local remap = vim.api.nvim_set_keymap
+    local npairs = require('nvim-autopairs')
+
+    npairs.setup({ map_bs = false, map_cr = false })
+
+    vim.g.coq_settings = { keymap = { recommended = false } }
+
+    -- these mappings are coq recommended mappings unrelated to nvim-autopairs
+    remap('i', '<esc>', [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
+    remap('i', '<c-c>', [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
+    remap('i', '<tab>', [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
+    remap('i', '<s-tab>', [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
+
+    -- skip it, if you use another global object
+    _G.MUtils= {}
+
+    MUtils.CR = function()
+      if vim.fn.pumvisible() ~= 0 then
+        if vim.fn.complete_info({ 'selected' }).selected ~= -1 then
+          return npairs.esc('<c-y>')
+        else
+          return npairs.esc('<c-e>') .. npairs.autopairs_cr()
+        end
+      else
+        return npairs.autopairs_cr()
+      end
+    end
+    remap('i', '<cr>', 'v:lua.MUtils.CR()', { expr = true, noremap = true })
+
+    MUtils.BS = function()
+      if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ 'mode' }).mode == 'eval' then
+        return npairs.esc('<c-e>') .. npairs.autopairs_bs()
+      else
+        return npairs.autopairs_bs()
+      end
+    end
+    remap('i', '<bs>', 'v:lua.MUtils.BS()', { expr = true, noremap = true })
+  end -- has('nvim-autopairs')
 end
 
 return M
