f418ebd5262af14de79edd52a3ad451491ae082f
Zachary Fogg <me@zfo.gg>
2026-02-04 09:11:00 -0500

    kitty

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/zfogg/.local/share/nvim/lazy/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/Users/zfogg/.local/share/nvim/lazy/fzf/bin"
+if [[ ! "$PATH" == */home/zfogg/.local/share/nvim/lazy/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/zfogg/.local/share/nvim/lazy/fzf/bin"
 fi
 
 source <(fzf --zsh)
--- .vim/init.lua
+++ .vim/init.lua
@@ -3,6 +3,15 @@
 vim.g.do_filetype_lua    = 1
 vim.g.did_load_filetypes = 0
 
+-- Define PHas early so plugin files can use it
+function _G.PHas(plugin)
+  local ok, lazy = pcall(require, "lazy.core.config")
+  if ok and lazy.plugins and lazy.plugins[plugin] then
+    return 1
+  end
+  return 0
+end
+
 vim.fn['z#rc#Init']()
 
 require('zfogg')
