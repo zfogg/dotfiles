184d93a6320bcff5040cd50762045e60ba8c888e
Zachary Fogg <me@zfo.gg>
2026-05-13 04:12:52 +0000

    zsh plugins antidote update i guess

--- .config/zsh/.zsh_plugins.zsh
+++ .config/zsh/.zsh_plugins.zsh
@@ -1,13 +1,13 @@
-fpath+=( "$HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-wookayin-SLASH-fast-syntax-highlighting" )
-source "$HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-wookayin-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
-fpath+=( "$HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search" )
-source "$HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh"
-fpath+=( "$HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions" )
-source "$HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
-source "$HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme"
-fpath+=( "$HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-willghatch-SLASH-zsh-hooks" )
-source "$HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-willghatch-SLASH-zsh-hooks/zsh-hooks.plugin.zsh"
-fpath+=( "$HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-softmoth-SLASH-zsh-vim-mode" )
-source "$HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-softmoth-SLASH-zsh-vim-mode/zsh-vim-mode.plugin.zsh"
-fpath+=( "$HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-agkozak-SLASH-zsh-z" )
-source "$HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-agkozak-SLASH-zsh-z/zsh-z.plugin.zsh"
+fpath+=( "$HOME/.local/share/antidote/github.com/wookayin/fast-syntax-highlighting" )
+source "$HOME/.local/share/antidote/github.com/wookayin/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+fpath+=( "$HOME/.local/share/antidote/github.com/zsh-users/zsh-history-substring-search" )
+source "$HOME/.local/share/antidote/github.com/zsh-users/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh"
+fpath+=( "$HOME/.local/share/antidote/github.com/zsh-users/zsh-autosuggestions" )
+source "$HOME/.local/share/antidote/github.com/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
+source "$HOME/.local/share/antidote/github.com/romkatv/powerlevel10k/powerlevel10k.zsh-theme"
+fpath+=( "$HOME/.local/share/antidote/github.com/willghatch/zsh-hooks" )
+source "$HOME/.local/share/antidote/github.com/willghatch/zsh-hooks/zsh-hooks.plugin.zsh"
+fpath+=( "$HOME/.local/share/antidote/github.com/softmoth/zsh-vim-mode" )
+source "$HOME/.local/share/antidote/github.com/softmoth/zsh-vim-mode/zsh-vim-mode.plugin.zsh"
+fpath+=( "$HOME/.local/share/antidote/github.com/agkozak/zsh-z" )
+source "$HOME/.local/share/antidote/github.com/agkozak/zsh-z/zsh-z.plugin.zsh"
